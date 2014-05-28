20c89edcf4d8a721b84d63f25d3cb417d6263a70
Loïc JACQUES <loic.jacques@volvo.com>
2014-05-28 14:12:16 +0200

    Added complete date and battery level

--- GlassDateDisplay/src/main/java/com/recursivepenguin/glassdatedisplay/DateDisplayService.java
+++ GlassDateDisplay/src/main/java/com/recursivepenguin/glassdatedisplay/DateDisplayService.java
@@ -1,127 +1,134 @@
 package com.recursivepenguin.glassdatedisplay;
 
-import android.animation.ObjectAnimator;
-import android.app.Notification;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.PixelFormat;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
-import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class DateDisplayService extends Service {
 
-    private WindowManager windowManager;
-    private TextView floatingDate;
-    BroadcastReceiver broadcastReceiver;
+	private WindowManager windowManager;
+	private TextView floatingDate;
+	private TextView battery;
+	private Intent batteryStatus;
+	private Runnable hide;
+	BroadcastReceiver broadcastReceiver;
+	private View mainView;
 
-    Timer timer;
+	Handler handler;
 
-    Handler handler;
+	@Override
+	public IBinder onBind(Intent intent) {
+		// Not used
+		return null;
+	}
 
-    @Override public IBinder onBind(Intent intent) {
-        // Not used
-        return null;
-    }
+	static SimpleDateFormat format = new SimpleDateFormat("EE dd MMM yyyy");
 
-    static SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+	@Override
+	public void onCreate() {
+		super.onCreate();
 
-    @Override public void onCreate() {
-        super.onCreate();
+		handler = new Handler(Looper.getMainLooper());
+		mainView = LayoutInflater.from(this).inflate(R.layout.main_layout, null);
+		floatingDate = (TextView) mainView.findViewById(R.id.date);
 
-        handler = new Handler(Looper.getMainLooper());
+		battery = (TextView) mainView.findViewById(R.id.batteryLvl);
 
-        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+		windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
 
-        floatingDate = new TextView(this);
-        floatingDate.setText(format.format(new Date()));
+		floatingDate.setText(format.format(new Date()));
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
+		WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+				WindowManager.LayoutParams.MATCH_PARENT,
+				WindowManager.LayoutParams.MATCH_PARENT,
+				WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+				PixelFormat.TRANSLUCENT);
 
-        params.gravity = Gravity.TOP | Gravity.CENTER;
-        params.x = 0;
-        params.y = 0;
+		windowManager.addView(mainView, params);
 
-        windowManager.addView(floatingDate, params);
+		hide = new Runnable() {
+			@Override
+			public void run() {
+				mainView.setVisibility(View.GONE);
 
-        broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context ctx, Intent intent)
-            {
-                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0) {
-                    floatingDate.setText(format.format(new Date()));
-                }
-            }
-        };
+			}
+		};
 
-        registerReceiver(broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+		broadcastReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context ctx, Intent intent) {
+				if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0) {
+					floatingDate.setText(format.format(new Date()));
+				} else if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+					int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+					battery.setText(String.valueOf(batteryLevel) + "%");
+				}
+			}
+		};
 
-        Notification notification = new Notification.Builder(this)
-                .setContentTitle("Glass time")
-                .getNotification();
+		registerReceiver(broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
 
-        startForeground(123, notification);
+		IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+		batteryStatus = registerReceiver(broadcastReceiver, ifilter);
 
-        scehduleFadeout();
+		int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		battery.setText(String.valueOf(batteryLevel) + "%");
 
-        registerReceiver(new BroadcastReceiver() {
+		//Useless if not  running on a phone
+//		Notification notification = new Notification.Builder(this)
+//				.setContentTitle("Glass time")
+//				.getNotification();
+//
+//		startForeground(123, notification);
 
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        ObjectAnimator.ofFloat(floatingDate, "alpha", 1f).start();
-                        scehduleFadeout();
-                    }
-                });
-            }
-        }, new IntentFilter(Intent.ACTION_SCREEN_ON));
-    }
+		scheduleFadeout();
 
-    private void scehduleFadeout() {
-        if (timer != null)
-            timer.cancel();
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        ObjectAnimator.ofFloat(floatingDate, "alpha", 0f).start();
-                    }
-                });
-            }
-        }, 3500);
-    }
+		registerReceiver(new BroadcastReceiver() {
 
-    @Override
-    public void onDestroy() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				mainView.setVisibility(View.VISIBLE);
+				scheduleFadeout();
 
-        stopForeground(true);
+			}
+		}, new IntentFilter(Intent.ACTION_SCREEN_ON));
+	}
 
-        if (broadcastReceiver != null)
-            unregisterReceiver(broadcastReceiver);
+	private void scheduleFadeout() {
 
-        super.onDestroy();
-        if (floatingDate != null) windowManager.removeView(floatingDate);
-    }
+		handler.removeCallbacksAndMessages(null);
+		handler.postDelayed(hide, 2500);
+
+	}
+
+	@Override
+	public void onDestroy() {
+
+		stopForeground(true);
+
+		if (broadcastReceiver != null) {
+			unregisterReceiver(broadcastReceiver);
+		}
+
+		super.onDestroy();
+		if (mainView != null) {
+			windowManager.removeView(mainView);
+		}
+	}
 
 }
